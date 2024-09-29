169bf82161891f781c38524c393c446311836d2b
vklepov <vklepov@ozon.ru>
2024-09-29 21:21:53 +0300

    reformat

--- src/data/clean.py
+++ src/data/clean.py
@@ -1,44 +1,56 @@
 import pandas as pd
 
+
 def clean_data():
-    df = pd.read_csv('./data/raw/itineraries_random_2M.csv')
+    df = pd.read_csv("./data/raw/itineraries_random_2M.csv")
 
     # remove low-frequency cases
-    is_premium = df['segmentsCabinCode'].str.contains('business') |\
-        df['segmentsCabinCode'].str.contains('premium') |\
-        df['segmentsCabinCode'].str.contains('first')
-    df = df[~is_premium & ~df['isRefundable']].copy()
-    df = df.drop(['segmentsCabinCode', 'isRefundable'], axis=1)
+    is_premium = (
+        df["segmentsCabinCode"].str.contains("business")
+        | df["segmentsCabinCode"].str.contains("premium")
+        | df["segmentsCabinCode"].str.contains("first")
+    )
+    df = df[~is_premium & ~df["isRefundable"]].copy()
+    df = df.drop(["segmentsCabinCode", "isRefundable"], axis=1)
     df = df.dropna()
 
     # drop duplicate cols
-    df = df.drop([
-        'segmentsAirlineName', 
-        'segmentsDepartureTimeRaw', 
-        'segmentsArrivalTimeRaw', 
-        'isBasicEconomy', 
-        'isNonStop',
-        'totalTravelDistance'
-    ], axis=1)
+    df = df.drop(
+        [
+            "segmentsAirlineName",
+            "segmentsDepartureTimeRaw",
+            "segmentsArrivalTimeRaw",
+            "isBasicEconomy",
+            "isNonStop",
+            "totalTravelDistance",
+        ],
+        axis=1,
+    )
     # drop irrelevans cols
-    df = df.drop([
-        'legId',
-        'fareBasisCode',
-        'searchDate',
-        'seatsRemaining',
-        'baseFare',
-        'segmentsEquipmentDescription',
-        'segmentsDistance'
-    ], axis=1)
+    df = df.drop(
+        [
+            "legId",
+            "fareBasisCode",
+            "searchDate",
+            "seatsRemaining",
+            "baseFare",
+            "segmentsEquipmentDescription",
+            "segmentsDistance",
+        ],
+        axis=1,
+    )
 
     # parse duration to minutes
     def parse_duration(i):
         if not i[2]:
             return int(i[1])
         return 60 * int(i[1]) + int(i[2])
-    df['travelDuration'] = df['travelDuration'].str.split(r'[^0-9]+').map(parse_duration)
 
-    df.to_csv('./data/interim/flights.csv', index=False)
+    df["travelDuration"] = (
+        df["travelDuration"].str.split(r"[^0-9]+").map(parse_duration)
+    )
+
+    df.to_csv("./data/interim/flights.csv", index=False)
 
 
 if __name__ == "__main__":
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from catboost import CatBoostRegressor
 
-target = 'totalFare'
+target = "totalFare"
+
 
 def train_model():
-    df_train = pd.read_csv('./data/processed/train.csv')
-    df_test = pd.read_csv('./data/processed/test.csv')
+    df_train = pd.read_csv("./data/processed/train.csv")
+    df_test = pd.read_csv("./data/processed/test.csv")
 
     X_train = df_train.drop(target, axis=1)
     y_train = df_train[target]
@@ -13,10 +14,10 @@ def train_model():
     y_test = df_test[target]
 
     cb_model = CatBoostRegressor(
-        cat_features=('startingAirport', 'destinationAirport')
-    ).fit(X_train, y_train, eval_set=(X_test,y_test))
+        cat_features=("startingAirport", "destinationAirport")
+    ).fit(X_train, y_train, eval_set=(X_test, y_test))
 
-    cb_model.save_model('./models/model.cbm')
+    cb_model.save_model("./models/model.cbm")
 
 
 if __name__ == "__main__":
