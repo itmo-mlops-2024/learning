a251b2187d5333612ffd6ee83b8042d22535ac01
Vladimir Klepov <v.klepov@gmail.com>
2024-10-01 00:04:12 +0300

    Add MLFlow remote tracking (#7)
    * Add mlflow cilent
    
    * Fix infer_signature hang
    
    * Use remote tracking server

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,8 +1,20 @@
+import os
+
 import click
+import mlflow
 import pandas as pd
 from catboost import CatBoostRegressor
+from dotenv import load_dotenv
+from mlflow.catboost import log_model
+from mlflow.models.signature import infer_signature
+from sklearn.metrics import mean_absolute_percentage_error, r2_score
+
+load_dotenv()
+remote_server_uri = os.getenv("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(remote_server_uri)
 
 target = "totalFare"
+SIGNATURE_SIZE = 10000
 
 
 @click.command()
@@ -10,19 +22,39 @@ target = "totalFare"
 @click.argument("in_test", type=click.Path(exists=True))
 @click.argument("output", type=click.Path())
 def train_model(in_train, in_test, output):
-    df_train = pd.read_csv(in_train)
-    df_test = pd.read_csv(in_test)
+    with mlflow.start_run():
+        mlflow.get_artifact_uri()
 
-    x_train = df_train.drop(target, axis=1)
-    y_train = df_train[target]
-    x_test = df_test.drop(target, axis=1)
-    y_test = df_test[target]
+        df_train = pd.read_csv(in_train)
+        df_test = pd.read_csv(in_test)
 
-    cb_model = CatBoostRegressor(
-        cat_features=("startingAirport", "destinationAirport")
-    ).fit(x_train, y_train, eval_set=(x_test, y_test))
+        x_train = df_train.drop(target, axis=1)
+        y_train = df_train[target]
+        x_test = df_test.drop(target, axis=1)
+        y_test = df_test[target]
 
-    cb_model.save_model(output)
+        params = {"cat_features": ("startingAirport", "destinationAirport")}
+        cb_model = CatBoostRegressor(**params).fit(
+            x_train, y_train, eval_set=(x_test, y_test)
+        )
+
+        y_pred = cb_model.predict(x_test)
+        score = {
+            "r2": r2_score(y_test, y_pred),
+            "mape": mean_absolute_percentage_error(y_test, y_pred),
+        }
+        signature = infer_signature(x_test[:SIGNATURE_SIZE], y_pred[:SIGNATURE_SIZE])
+
+        mlflow.log_params(params)
+        mlflow.log_metrics(score)
+        log_model(
+            cb_model,
+            artifact_path="model",
+            registered_model_name="plane_price_catboost",
+            signature=signature,
+        )
+
+        cb_model.save_model(output)
 
 
 if __name__ == "__main__":
