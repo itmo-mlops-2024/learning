481852a7a5048aed223b2b4da85abfa3e231f6aa
vklepov <vklepov@ozon.ru>
2024-09-30 19:53:20 +0300

    Fix infer_signature hang

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,16 +4,15 @@ import click
 import mlflow
 import pandas as pd
 from catboost import CatBoostRegressor
-from dotenv import load_dotenv
 from mlflow.catboost import log_model
 from mlflow.models.signature import infer_signature
 from sklearn.metrics import mean_absolute_percentage_error, r2_score
 
-load_dotenv()
 remote_server_uri = os.getenv("MLFLOW_TRACKING_URI")
 mlflow.set_tracking_uri(remote_server_uri)
 
 target = "totalFare"
+SIGNATURE_SIZE = 10000
 
 
 @click.command()
@@ -32,27 +31,26 @@ def train_model(in_train, in_test, output):
         x_test = df_test.drop(target, axis=1)
         y_test = df_test[target]
 
-        params = {
-            'cat_features': ("startingAirport", "destinationAirport")
-        }
-        cb_model = CatBoostRegressor(
-            **params
-        ).fit(x_train, y_train, eval_set=(x_test, y_test))
+        params = {"cat_features": ("startingAirport", "destinationAirport")}
+        cb_model = CatBoostRegressor(**params).fit(
+            x_train, y_train, eval_set=(x_test, y_test)
+        )
 
         y_pred = cb_model.predict(x_test)
         score = {
             "r2": r2_score(y_test, y_pred),
             "mape": mean_absolute_percentage_error(y_test, y_pred),
         }
-        signature = infer_signature(x_test, y_pred)
+        signature = infer_signature(x_test[:SIGNATURE_SIZE], y_pred[:SIGNATURE_SIZE])
 
         mlflow.log_params(params)
         mlflow.log_metrics(score)
-        log_model(lgb_model=cb_model,
-                                  artifact_path="model",
-                                  registered_model_name="plane_price_catboost",
-                                  signature=signature)
-
+        log_model(
+            cb_model,
+            artifact_path="model",
+            registered_model_name="plane_price_catboost",
+            signature=signature,
+        )
 
         cb_model.save_model(output)
 
